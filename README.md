79618385a65d0203d03ef8e4394c0bd7feeba796
altbdoor <lancersupraskyline@gmail.com>
2014-01-05 14:43:36 +0800

    version 2 on the roll

--- README.md
+++ README.md
@@ -1,22 +1,39 @@
 pxDrag (pixelly Dragging)
-=========================
+===
 
-Version 1.0   
-Last updated 25 September 2013   
+Version 2.0
 
-For more information and test drive, check out the [development page](//googledrive.com/host/0B5pMzfAiZLn4Q2ZFZnZZdWU4OGs/pxDrag.html).
+
+Last updated 5 January 2014   
+
 
 About
------
+---
 pxDrag is a small JavaScript for object panning or "drag and drop" within a boundary. It was made after a number of frustrated attempts in using image panning plugins.
 
 
-Browser Support
----------------
-- Internet Explorer 8+
-- Firefox 22.0+
-- Chrome 22.0+
-- Safari 5.1+
-- Opera 16.0+
+Usage
+--
+```
+pxDrag({
+  child: element_in_effect,
+  parent: parent_of_element_in_effect,
+  mode: "pan" || "move"
+});
+```
 
-Please note that the browser support is rather demanding, as it uses `getComputedStyle` to determine the initial margin values of the target element. To support older browsers, you could define the margin values immediately with the style attribute, although it is not recommended.
+`child` is basically the element in effect, which is to be panned or dragged around a boundary. `child` is compulsory and must be defined.
+
+This boundary is set from the `parent` element, which can be specified, or automatically assumed to be the `child`'s immediate parent node. `parent` is optional and can be left undefined.
+
+The `mode` takes in a string, of either "pan" or "move". This allows the `mode` to be overridden. If undefined, the mode will be automatically assumed. If the `child`'s dimensions are larger than its `parent`, it will be "pan". Otherwise, "move". `mode` is optional and can be left undefined.
+
+
+Browser Support
+---
+No thorough testing were made, but it was written to cover the widest base possible (with my limited skills), including IE8.
+
+
+License
+---
+MIT
--- pxDrag.js
+++ pxDrag.js
@@ -1,153 +1,160 @@
 /*
- * pixelly dragging (pxDrag) version 1.0
- * http://googledrive.com/host/0B5pMzfAiZLn4Q2ZFZnZZdWU4OGs/pxDrag.html
+ * pxDrag (pixelly Dragging) version 2.0
+ * https://github.com/altbdoor/pxDrag
  *
- * Copyright (C) 2013 Ng Han Seng (altbdoor)
+ * Copyright (C) 2014 altbdoor
  * Released under the MIT license
  * http://opensource.org/licenses/MIT
  */
 
-var pxDrag = function (child, parent, mode) {
-	/*
-		preventDefault, addEventListener and removeEventListener polyfill for IE 8 and 9
-		Edited and condensed from http://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
-	*/
-	(function () {
-		if (!Event.prototype.preventDefault) {
-			Event.prototype.preventDefault = function () {
-				this.returnValue=false;
-			};
-		}
+var pxDrag = (function () {
+	// polyfills
+	var _preventDefault, _stopPropagation,
+		_addEventListener, _removeEventListener,
 		
-		if (!Event.prototype.stopPropagation) {
-			Event.prototype.stopPropagation = function () {
-				this.cancelBubble=true;
-			};
-		}
-		
-		if (!Element.prototype.addEventListener) {
-			var eventListeners = [];
-			
-			var addEventListener = function (type, listener) {
-				var self = this,
-					wrapper = function (e) {
-						e.target = e.srcElement;
-						e.currentTarget = self;
-						if (listener.handleEvent) {
-							listener.handleEvent(e);
-						} else {
-							listener.call(self,e);
-						}
-					};
-				
-				this.attachEvent("on"+type, wrapper);
-				eventListeners.push({object:this,type:type,listener:listener,wrapper:wrapper});
-			}
-			
-			var removeEventListener = function (type, listener) {
-				for (var counter=0; counter<eventListeners.length; counter++) {
-					var eventListener = eventListeners[counter];
-					if (eventListener.object==this && eventListener.type==type && eventListener.listener==listener) {
-						this.detachEvent("on"+type, eventListener.wrapper);
-						break;
-					}
-				}
-			}
-			
-			Element.prototype.addEventListener = addEventListener;
-			Element.prototype.removeEventListener = removeEventListener;
-			
-			if (HTMLDocument) {
-				HTMLDocument.prototype.addEventListener = addEventListener;
-				HTMLDocument.prototype.removeEventListener = removeEventListener;
-			}
-			if (Window) {
-				Window.prototype.addEventListener = addEventListener;
-				Window.prototype.removeEventListener = removeEventListener;
-			}
-		}
-	})();
+		standardizeEvent = function (e) {
+			return e || window.event;
+		};
 	
-	// get all elements ready, and calculate max boundary
-	var _child = child,
-		_parent = ((parent) ? parent : _child.parentNode),
-		
-		maxBoundX = _parent.clientWidth - _child.offsetWidth,
-		maxBoundY = _parent.clientHeight - _child.offsetHeight,
-		
-		mode = ((mode) ? mode : ((maxBoundX <= 0 || maxBoundY <= 0) ? 'pan' : 'move'));
-	
-	// set the object to bind
-	if (mode == 'pan') {
-		toBind = _parent;
+	// event.preventDefault
+	if (Event.prototype.preventDefault) {
+		_preventDefault = function (e) {
+			e.preventDefault();
+		}
 	}
-	else if (mode == 'move') {
-		toBind = _child;
+	else {
+		_preventDefault = function (e) {
+			e = standardizeEvent(e);
+			e.returnValue = false;
+		}
 	}
 	
-	// object clicked, ready to drag
-	toBind.addEventListener('mousedown', function (e) {
-		// get the event, mouse position and initial margin values
-		var e = e || window.event,
+	// event.stopPropagation
+	if (Event.prototype.stopPropagation) {
+		_stopPropagation = function (e) {
+			e.stopPropagation();
+		}
+	}
+	else {
+		_stopPropagation = function (e) {
+			e = standardizeEvent(e);
+			e.cancelBubble = true;
+		}
+	}
+	
+	// event.addEventListener and event.removeEventListener
+	if (Element.prototype.addEventListener) {
+		_addEventListener = function (elem, type, listener) {
+			elem.addEventListener(type, listener, false);
+		}
 		
-			posX = e.clientX,
-			posY = e.clientY;
+		_removeEventListener = function (elem, type, listener) {
+			elem.removeEventListener(type, listener, false);
+		}
+	}
+	else {
+		_addEventListener = function (elem, type, listener) {
+			elem.attachEvent('on' + type, listener);
+		};
+		
+		_removeEventListener = function (elem, type, listener) {
+			elem.detachEvent('on' + type, listener);
+		};
+	}
+	
+	// the function
+	return function (obj) {
+		// get all elements ready, and calculate max boundary
+		var _child = obj.child,
+			_parent = ((obj.parent) ? obj.parent : _child.parentNode),
 			
-		e.stopPropagation();
+			maxBoundX = _parent.clientWidth - _child.offsetWidth,
+			maxBoundY = _parent.clientHeight - _child.offsetHeight,
+			
+			toBind, _mode;
 		
-		if (document.all && document.querySelector) {
-			var childX = (_child.style.marginLeft || _child.currentStyle.marginLeft).replace('px', ''),
-				childY = (_child.style.marginTop || _child.currentStyle.marginTop).replace('px', '');
+		// set object to bind
+		if (obj.mode) {
+			_mode = obj.mode;
+			
+			if (obj.mode === 'pan') {
+				toBind = _parent;
+			}
+			else if (obj.mode === 'move') {
+				toBind = _child;
+			}
 		}
 		else {
-			var childX = (_child.style.marginLeft || window.getComputedStyle(_child).marginLeft).replace('px', ''),
-				childY = (_child.style.marginTop || window.getComputedStyle(_child).marginTop).replace('px', '');
-		}
-			
-		var diffX = posX - childX,
-			diffY = posY - childY;
-		
-		// function for document when object is being dragged
-		function docMouseMove (e) {
-			var e = e || window.event,
-		
-				nextPosX = e.clientX,
-				nextPosY = e.clientY,
-				
-				nextChildX = nextPosX - diffX,
-				nextChildY = nextPosY - diffY;
-				
-			document.body.style.cursor = 'move';
-			
-			if (maxBoundX <= 0 || maxBoundY <= 0) {
-				_child.style.marginLeft = Math.max(Math.min(0, nextChildX), maxBoundX) + 'px';
-				_child.style.marginTop = Math.max(Math.min(0, nextChildY), maxBoundY) + 'px';
+			if (maxBoundX <= 0 && maxBoundY <= 0) {
+				_mode = 'pan';
+				toBind = _parent;
 			}
 			else {
-				_child.style.marginLeft = Math.max(0, Math.min(maxBoundX, nextChildX)) + 'px';
-				_child.style.marginTop = Math.max(0, Math.min(maxBoundY, nextChildY)) + 'px';
+				_mode = 'move';
+				toBind = _child;
 			}
 		}
 		
-		// function for document to prevent selection while dragging
-		function docSelectStart (e) {
-			e.preventDefault();
-		}
-		
-		// bind function to objects
-		document.addEventListener('mousemove', docMouseMove, false);
-		document.addEventListener('selectstart', docSelectStart, false);
-		
-		// function to remove the events after finished dragging
-		document.addEventListener('mouseup', function (e) {
-			document.body.style.cursor = '';
-			document.removeEventListener('mousemove', docMouseMove, false);
-			document.removeEventListener('selectstart', docSelectStart, false);
+		// object click, ready to drag
+		_addEventListener(toBind, 'mousedown', function (e) {
+			var e = standardizeEvent(e),
+				
+				childX, childY,
+				diffX, diffY;
+			
+			if (_child.style.marginLeft) {
+				childX = +_child.style.marginLeft.replace('px', '');
+			}
+			else {
+				childX = _child.getBoundingClientRect().left - _parent.getBoundingClientRect().left;
+			}
+			
+			if (_child.style.marginTop) {
+				childY = +_child.style.marginTop.replace('px', '');
+			}
+			else {
+				childY = _child.getBoundingClientRect().top - _parent.getBoundingClientRect().top;
+			}
+			
+			diffX = e.clientX - childX;
+			diffY = e.clientY - childY;
+			
+			_stopPropagation(e);
+			
+			// prevent selection
+			function elemSelectStart (e) {
+				_preventDefault(e);
+			}
+			_addEventListener(document, 'selectstart', elemSelectStart);
+			
+			// move object
+			function elemMouseMove (e) {
+				var e = standardizeEvent(e),
+					
+					nextX = e.clientX - diffX,
+					nextY = e.clientY - diffY;
+				
+				if (_mode === 'pan') {
+					_child.style.marginLeft = Math.max(Math.min(0, nextX), maxBoundX) + 'px';
+					_child.style.marginTop = Math.max(Math.min(0, nextY), maxBoundY) + 'px';
+				}
+				else if (_mode === 'move') {
+					_child.style.marginLeft = Math.max(0, Math.min(maxBoundX, nextX)) + 'px';
+					_child.style.marginTop = Math.max(0, Math.min(maxBoundY, nextY)) + 'px';
+				}
+			}
+			_addEventListener(document, 'mousemove', elemMouseMove);
+			
+			// function to remove the events after finished dragging
+			_addEventListener(document, 'mouseup', function () {
+				_removeEventListener(document, 'selectstart', elemSelectStart);
+				_removeEventListener(document, 'mousemove', elemMouseMove);
+			});
+			
+			// prevent default browser action of dragging
+			_addEventListener(_parent, 'dragstart', function (e) {
+				_preventDefault(e);
+			});
 		});
-		
-		// prevent default browser action of dragging
-		_parent.addEventListener('dragstart', function (e) {
-			e.preventDefault();
-		}, false);
-	}, false);
-}
+	};
+})();
